1e0f17108485fef5bba775a5922f9983df7f474f
lookingfrag <jessesimanjuntaks@gmail.com>
2024-06-26 15:24:21 +0700

    feat : jumb jum tok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,28 @@ import NextImage from '@/components/NextImage';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-1">
+    <main className="flex min-h-screen py-24 bg-blue-1">
+      
       <Navbar />
 
-        <h2 className='font-lemonMilk text-[30px] font-medium text-blue-3 my-5'> ALL TOP UP PRODUCTS </h2>
-
-      <div className='flex flex-wrap gap-6'>
-      <Card title=""/>
+      <div className="flex px-[55px] py-[75px] min-h-screen flex-col">
+        <div className="flex min-h-screen flex-col">
+          <a className="font-poppins text-[30px] font-medium text-blue-3 font-bold">
+            BACUEMI{" "}
+          </a>
+          <a className="font-lemonMilk text-[80px] font-medium text-blue-3">
+            UPGRADE YOUR{" "}<br/> TOP UP EXPERIENCE{" "}
+          </a>
+          <a className="font-poppins text-[30px] font-medium text-blue-3">
+            Easy, Fast, and Secure
+          </a>
+        </div>
+        <div className="absolute z-1 w-[40%] right-[0%] top-[275%] md:top-[15%]">
+          <img src="/img/jett.png" alt="Jett"/>
+        </div>
+        <div className="absolute z-0 w-[100%] left-[0%] md:top-[85%]">
+          <img src="/img/kunai.png" alt="kunai"/>
+        </div>
       </div>
 
     </main>
